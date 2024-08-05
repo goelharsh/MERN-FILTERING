87a22c98fbd404032b3a12a156e7541c892c14b3
napharsh <harsh.goel@napworks.in>
2024-08-05 21:40:13 +0530

    Render link updated at backend

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ connectDB().then(() => {
     app.use(express.json());
     app.use(
         cors({
-            origin: process.env.FRONTEND_URL || "http://localhost:5173",
+            origin: "https://mern-filtering.onrender.com/api" || "http://localhost:5173",
             credentials: true
         })
     );
